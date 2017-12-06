676464fb8a312898970cfb3743386e791e16b8d1
diasozo <diasozo@icloud.com>
2017-12-06 13:34:51 -0600

    minor file changes/additions

--- updateXRT.sh
+++ updateXRT.sh
@@ -5,6 +5,12 @@ echo "Pulling updates from master branch of kklm/xrt on github"
 git fetch upstream
 echo "Ensuring that the master branch is selected"
 git checkout master
+echo "adding any changes files"
+git add --all
+echo "Committing changes with generic message"
+git commit -m "minor file changes/additions"
+echo "Pushing changes to 'master' branch"
+git push origin master
 echo "Re-writing master branch to include updates"
 git rebase upstream/master
 echo "Pushing back updates to diasozo/xrt"
